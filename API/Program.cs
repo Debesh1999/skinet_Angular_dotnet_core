bf91fdca31b629ca2648a55a149b52e6ab82e0ef
Debesh1999 <69162539+Debesh1999@users.noreply.github.com>
2025-03-28 00:37:02 +0530

    Third commit

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure;
 using Infrastructure.Data;
@@ -16,12 +17,18 @@ builder.Services.AddDbContext<StoreContext>(opt => {
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddCors();
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+ 
+ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+     .WithOrigins("http://localhost:4200","https://localhost:4200"));
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
